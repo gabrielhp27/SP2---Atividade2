67d4d3647b42044f253322f5c3cad39a8bb8c9c4
gabrielhp27 <97366135+gabrielhp27@users.noreply.github.com>
2023-09-06 21:13:46 -0300

    Add files via upload

---
+++ atividadeCFE - inicial para atividade/js/jquery-script.js
@@ -0,0 +1,11 @@
+//efeito de esconder formulário de cadastro
+$(document).ready(function(){
+
+$("#botao-cadastrar").click(function(){
+
+    $("#botao-cadastrar").slideToggle("slow")
+
+})
+
+
+})
---
+++ atividadeCFE - inicial para atividade/teste.js/scripteste.js
@@ -0,0 +1,7 @@
+
+
+//função com argumento e com retorno
+function media(notaum, notadois){
+return(notaum + notadois) /2;
+}
+console.log("A média é:" + media(9,5));
